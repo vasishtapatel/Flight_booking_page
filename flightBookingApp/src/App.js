b1992bed545b8399d73f44efce6762d4e9a5488e
vasishtapatel <55794905+vasishtapatel@users.noreply.github.com>
2022-11-14 12:56:09 +0530

    Add files via upload

---
+++ flightBookingApp/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import FlightApp from './components/FlightApp'
+
+
+
+
+
+function App() {
+  return (
+    <React.Fragment >
+      <FlightApp/>
+                         
+    </React.Fragment>
+     
+       
+  
+  );
+}
+
+export default App;
